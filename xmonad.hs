67b94b4d9346e0cd0a055e7cd3bf4354e8700831
Paul Dawson <excidere@gmail.com>
2013-12-05 07:39:16 -0600

    cleaned up some whitespace issues

--- xmonad.hs
+++ xmonad.hs
@@ -6,22 +6,22 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 myManageHook = composeAll
-    	     [ className =? "Gimp"  --> doFloat
-    	     ]
+             [ className =? "Gimp"  --> doFloat
+             ]
 
 main = do
      xmproc <- spawnPipe "/usr/bin/xmobar /home/piisalie/dotfiles/xmobarrc"
      xmonad $ defaultConfig {
-     	    terminal          = "terminator",
-     	    modMask           = mod4Mask,
-     	    borderWidth       = 1,
-     	    focusFollowsMouse = False,
-     	    manageHook        = manageDocks <+> myManageHook
-	    		        <+> manageHook defaultConfig,
-     	    layoutHook        = avoidStruts  $  layoutHook defaultConfig,
-	    logHook           = dynamicLogWithPP xmobarPP {
-	    		      ppOutput = hPutStrLn xmproc,
-			      ppOrder  = \(ws:_:t:_) -> [ws,t],
-	                      ppTitle  = xmobarColor "orange" "" . shorten 50
+            terminal          = "terminator",
+            modMask           = mod4Mask,
+            borderWidth       = 1,
+            focusFollowsMouse = False,
+            manageHook        = manageDocks <+> myManageHook
+                                <+> manageHook defaultConfig,
+            layoutHook        = avoidStruts  $  layoutHook defaultConfig,
+            logHook           = dynamicLogWithPP xmobarPP {
+                              ppOutput = hPutStrLn xmproc,
+                              ppOrder  = \(ws:_:t:_) -> [ws,t],
+                              ppTitle  = xmobarColor "orange" "" . shorten 50
                               }
-      	    }
+            }
